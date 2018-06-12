a6770af29e5e51b96df7272d0a7857a8660fd7f1
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-12 22:53:58 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  array.collect { |x| x ** 2 }
+  new_array = []
+  array.each do |number|
+    new_number = number ** 2
+    new_array << new_number
+  end
+  new_array
 end
